f94e19049d68bd7faf8f39b1872ef647166b106c
jbermejo14 <jpbermejobramley@gmail.com>
2024-05-18 10:36:43 +0200

    cambios

--- src/main/java/org/bmbgarage/dao/CarDao.java
+++ src/main/java/org/bmbgarage/dao/CarDao.java
@@ -24,11 +24,11 @@ public interface CarDao {
     @UseRowMapper(CarMapper.class)
     List<Car> getCars(@Bind("searchTerm") String searchTerm);
 
-    @SqlUpdate("INSERT INTO cars (license_plate, brand, carmodel, dateregistration, price, image) VALUES (?, ?, ?, ?, ?, ?)")
-    int addCar(String license_plate, String brand, String carmodel, int dateregistration, float price, String image);
+    @SqlUpdate("INSERT INTO cars (licenseplate, brand, carmodel, dateregistration, price, image) VALUES (?, ?, ?, ?, ?, ?)")
+    int addCar(String licenseplate, String brand, String carmodel, int dateregistration, float price, String image);
 
-    @SqlUpdate("UPDATE cars SET license_plate = ?, brand = ?, carmodel = ?, dateregistration = ?, price = ?, image = ? WHERE id = ?")
-    int updateCar(String license_plate, String brand, String carmodel, int dateregistration, float price, String image, int id);
+    @SqlUpdate("UPDATE cars SET licenseplate = ?, brand = ?, carmodel = ?, dateregistration = ?, price = ?, image = ? WHERE id = ?")
+    int updateCar(String licenseplate, String brand, String carmodel, int dateregistration, float price, String image, int id);
 
     @SqlUpdate("DELETE FROM cars WHERE id = ?")
     int removeCar(int id);
